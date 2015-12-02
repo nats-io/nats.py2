0c7db2d466b61008ff60dbd038856bbaa7812e45
Waldemar Quevedo <wally@apcera.com>
2015-12-01 23:22:05 -0800

    Example updates

--- examples/example.py
+++ examples/example.py
@@ -8,23 +8,21 @@ def main():
     nc = NATS()
 
     # Establish connection to the server.
-    yield nc.connect({ "verbose": True, "servers": ["nats://127.0.0.1:4225"] })
+    yield nc.connect({ "verbose": True, "servers": ["nats://127.0.0.1:4222"] })
 
-    def discover(msg):
+    def discover(msg=None):
         print("[Received]: %s" % msg.data)
 
     sid = yield nc.subscribe("discover", "", discover)
     yield nc.auto_unsubscribe(sid, 2)
 
     loop = tornado.ioloop.IOLoop.instance()
-    yield tornado.gen.Task(loop.add_timeout, time.time() + 1)
-
     yield nc.publish("discover", "A")
     yield nc.publish("discover", "B")
-    
+
     # Following two messages won't be received.
     yield nc.publish("discover", "C")
-    yield nc.publish("discover", "D")    
+    yield nc.publish("discover", "D")
 
     def help_request_handler(msg):
         print("[Received]: %s" % msg.data)
@@ -33,7 +31,7 @@ def main():
     yield nc.subscribe("help", "workers", help_request_handler)
     response = yield nc.timed_request("help", "Hi, need help!")
     print("[Response]: %s" % response.data)
-    
+
     yield tornado.gen.Task(loop.add_timeout, time.time() + 1)
 
 if __name__ == '__main__':
