a389340d78438e9e9a99679ee28b22405e298886
Waldemar Quevedo <wally@apcera.com>
2015-12-22 15:07:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from nats.io.client import __version__
 
 setup(
-  name='nats',
+  name='nats-client',
   version=__version__,
   description='NATS client for Python 2',
   long_description='Python 2 client for NATS, a lightweight, high-performance cloud native messaging system',
@@ -10,7 +10,7 @@ setup(
   author='Waldemar Quevedo',
   author_email='wally@apcera.com',
   license='MIT License',
-  packages=['nats'],
+  packages=['nats', 'nats.io', 'nats.protocol'],
   install_requires=[
     'tornado',
   ],
