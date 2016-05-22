e8ffc82f6f0b3940d27f33f2bb8b22bbfe663d0a
Waldemar Quevedo <wally@apcera.com>
2016-05-21 20:18:16 -0700

    Avoid loading the nats client version in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@ from setuptools import setup, find_packages
 from nats import __version__
 
 setup(
-  name='nats-client',
-  version=__version__,
-  description='NATS client for Python 2',
-  long_description='Python 2 client for NATS, a lightweight, high-performance cloud native messaging system',
-  url='https://github.com/nats-io/python-nats',
-  author='Waldemar Quevedo',
-  author_email='wally@apcera.com',
-  license='MIT License',
-  packages=find_packages(),
-  install_requires=['tornado==4.2'],
-  zip_safe=True,
+    name='nats-client',
+    version=__version__,
+    description='NATS client for Python 2',
+    long_description='Tornado based Python client for NATS, a lightweight, high-performance cloud native messaging system',
+    url='https://github.com/nats-io/python-nats',
+    author='Waldemar Quevedo',
+    author_email='wally@apcera.com',
+    license='MIT License',
+    packages=['nats', 'nats.io', 'nats.protocol'],
+    install_requires=['tornado==4.2'],
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ]
 )
