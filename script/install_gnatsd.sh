309389ebbc584585613c9151fbae752f83869e8c
Waldemar Quevedo <wally@apcera.com>
2016-10-01 18:58:17 -0700

    Fix travis script

--- script/install_gnatsd.sh
+++ script/install_gnatsd.sh
@@ -10,9 +10,9 @@ if [ ! "$(ls -A $HOME/nats-server)" ]; then
     (
 	mkdir -p $HOME/nats-server
 	cd $HOME/nats-server
-	wget https://github.com/nats-io/gnatsd/releases/download/$NATS_SERVER_VERSION/gnatsd-$NATS_SERVER_VERSION-linux-amd64.zip -O nats-server.zip
-	unzip nats-server.zip
-	cp gnatsd-$NATS_SERVER_VERSION-linux-amd64/gnatsd $HOME/nats-server/gnatsd
+	wget https://github.com/nats-io/gnatsd/releases/download/$NATS_SERVER_VERSION/gnatsd-$NATS_SERVER_VERSION-linux-amd64.zip -O gnatsd.zip
+	unzip gnatsd.zip
+	mv gnatsd-$NATS_SERVER_VERSION-linux-amd64/gnatsd .
     )
 else
   echo 'Using cached directory.';
