f7aa05eab253ff5ff16923813683eb47e3665ad2
Waldemar Quevedo <wally@apcera.com>
2016-10-01 19:02:50 -0700

    More travis fixes...

--- script/install_gnatsd.sh
+++ script/install_gnatsd.sh
@@ -6,10 +6,10 @@ export DEFAULT_NATS_SERVER_VERSION=v0.9.4
 export NATS_SERVER_VERSION="${NATS_SERVER_VERSION:=$DEFAULT_NATS_SERVER_VERSION}"
 
 # check to see if gnatsd folder is empty
-if [ ! "$(ls -A $HOME/nats-server)" ]; then
+if [ ! "$(ls -A $HOME/gnatsd)" ]; then
     (
-	mkdir -p $HOME/nats-server
-	cd $HOME/nats-server
+	mkdir -p $HOME/gnatsd
+	cd $HOME/gnatsd
 	wget https://github.com/nats-io/gnatsd/releases/download/$NATS_SERVER_VERSION/gnatsd-$NATS_SERVER_VERSION-linux-amd64.zip -O gnatsd.zip
 	unzip gnatsd.zip
 	mv gnatsd-$NATS_SERVER_VERSION-linux-amd64/gnatsd .
