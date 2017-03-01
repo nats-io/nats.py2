746e22e4e9cd4ade06c33589fb0e3180f3b85d3a
Waldemar Quevedo <wally@apcera.com>
2017-03-01 01:45:30 -0800

    Update reconnecting tests with max_reconnect_attempts

--- tests/client_test.py
+++ tests/client_test.py
@@ -285,20 +285,27 @@ class ClientTest(tornado.testing.AsyncTestCase):
                def __init__(self):
                     self.nc = Client()
                     self.disconnected_cb_called = False
+                    self.closed_cb_called = False
 
                def disconnected_cb(self):
                     self.disconnected_cb_called = True
+
+               def closed_cb(self):
+                    self.closed_cb_called = True
 
           client = SampleClient()
           with self.assertRaises(ErrNoServers):
                options = {
                     "servers": ["nats://127.0.0.1:4223"],
-                    "close_cb": client.disconnected_cb,
+                    "disconnected_cb": client.disconnected_cb,
+                    "close_cb": client.closed_cb,
                     "allow_reconnect": True,
-                    "io_loop": self.io_loop
+                    "io_loop": self.io_loop,
+                    "max_reconnect_attempts": 2
                     }
                yield client.nc.connect(**options)
           self.assertFalse(client.disconnected_cb_called)
+          self.assertFalse(client.closed_cb_called)
 
      @tornado.testing.gen_test(timeout=5)
      def test_connect_fails_allow_reconnect_forever_until_close(self):
@@ -960,7 +967,9 @@ class ClientAuthTest(tornado.testing.AsyncTestCase):
                "close_cb": component.close_cb,
                "error_cb": component.error_cb,
                "disconnected_cb": component.disconnected_cb,
-               "reconnected_cb": component.reconnected_cb
+               "reconnected_cb": component.reconnected_cb,
+               "max_reconnect_attempts": 10,
+               "reconnect_time_wait": 0.1
                }
           yield component.nc.connect(**options)
           self.assertEqual(True, component.nc.is_connected)
@@ -1060,6 +1069,7 @@ class ClientAuthTest(tornado.testing.AsyncTestCase):
                "reconnected_cb": c.reconnected_cb,
                "disconnected_cb": c.disconnected_cb,
                "error_cb": c.error_cb,
+               "reconnect_time_wait": 0.1
                }
           yield c.nc.connect(**options)
           self.assertEqual(True, nc._server_info["auth_required"])
@@ -1068,18 +1078,16 @@ class ClientAuthTest(tornado.testing.AsyncTestCase):
           yield c.nc.subscribe("foo",  "", log.persist)
           self.io_loop.spawn_callback(c.publisher)
 
+          a = nc._current_server
           yield tornado.gen.sleep(0.001)
           orig_gnatsd = self.server_pool.pop(0)
           orig_gnatsd.finish()
           yield tornado.gen.sleep(0.001)
 
-          try:
-               a = nc._current_server
-               # Wait for reconnect logic kick in...
-               yield tornado.gen.sleep(3)
-          finally:
-               b = nc._current_server
-               self.assertNotEqual(a.uri, b.uri)
+          # Wait for reconnect logic kick in...
+          yield tornado.gen.sleep(3)
+          b = nc._current_server
+          self.assertNotEqual(a.uri, b.uri)
 
           # Should have reconnected already
           self.assertTrue(nc.is_connected)
